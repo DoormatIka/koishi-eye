4a48caa89de87c2ca2c1f682b01c37c02762b7e7
DoormatIka <yomotouhou@gmail.com>
2026-01-29 19:43:59 +0800

    multithreading

--- src/hasher.py
+++ src/hasher.py
@@ -1,10 +1,11 @@
 
 from dataclasses import dataclass
+import os
 from typing import Protocol
 import numpy as np
 import imagehash
 
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from PIL.Image import Image as PILImage
 from concurrent.futures import ProcessPoolExecutor, Future, as_completed
 from pathlib import Path
@@ -45,46 +46,70 @@ class BruteForceFinder:
         exts = {"png", "jpg", "jpeg"}
         image_hashes: list[CombinedImageHash] = list()
 
-        n_thread = 4
+        n_thread = os.cpu_count()
+        if n_thread == None:
+            raise ValueError("OS cpu count cannot be found!")
+
+        n_thread = max(n_thread, 2)
+        n_thread = 1
         with ProcessPoolExecutor(max_workers=n_thread) as executor:
-            futures: list[Future[CombinedImageHash]] = list()
+            futures: list[Future[ImageHashResult]] = list()
             for ext in exts:
                 for image_path in Path(directory).rglob(f"*.{ext}"):
                     futures.append(executor.submit(self.hasher.create_hash_from_image, image_path))
 
             for future in as_completed(futures):
-                result = future.result()
-                image_hashes.append(result)
-                self.hasher.logger.info(f"hashing \"{result.path}\"")
+                result, err = future.result()
+                if result != None:
+                    self.hasher.logger.info(f"hashing \"{result.path}\"")
+                    image_hashes.append(result)
+                else:
+                    self.hasher.logger.warn(err or "")
 
         image_hashes.sort(key=lambda x: imagehash_to_int(x.hash))
 
         return image_hashes
 
-    def get_similar_images(self, image_hashes: list[CombinedImageHash]) -> list[list[CombinedImageHash]]:
-        nearest_matches: list[list[CombinedImageHash]] = list()
-        MATCH_THRESHOLD = 5
 
-        for i, img1 in enumerate(image_hashes):
-            for img2 in image_hashes[i + 1:]:
-                if img1.path == img2.path:
+    def get_similar_images(self, image_hashes: list[CombinedImageHash]) -> list[ImagePair]:
+        nearest_matches: list[ImagePair] = list()
+
+        with ProcessPoolExecutor(max_workers=1) as executor:
+            futures: list[Future[ImagePair | None]] = list()
+            for i, img1 in enumerate(image_hashes):
+                for img2 in image_hashes[i + 1:]:
+                    futures.append(executor.submit(is_similar_image, img1, img2))
+
+            for future in as_completed(futures):
+                val = future.result()
+                if val == None:
                     continue
-                if abs(img1.hash - img2.hash) < MATCH_THRESHOLD:
-                    nearest_matches.append([img1, img2])
-                    matching_segments, distance = img1.cropped_hash.hash_diff(img2.cropped_hash)
+                img1, img2 = val
+                matching_segments, distance = img1.cropped_hash.hash_diff(img2.cropped_hash)
+                self.hasher.logger.match(
+                    f"\tLeft: {img1.path}\n" + 
+                    f"\tRight: {img2.path}\n" + 
+                    f"\tGlobal Difference: {abs(img1.hash - img2.hash)}\n" +
+                    f"\tCropped Difference: (matching_segments: {matching_segments}, distance: {distance})\n"
+                )
+                nearest_matches.append((img1, img2))
 
-                    self.hasher.logger.match(
-                        f"\tLeft: {img1.path}\n" + 
-                        f"\tRight: {img2.path}\n" + 
-                        f"\tGlobal Difference: {abs(img1.hash - img2.hash)}\n" +
-                        f"\tCropped Difference: (matching_segments: {matching_segments}, distance: {distance})\n"
-                    )
+
 
         return nearest_matches
+
+def is_similar_image(img1: CombinedImageHash, img2: CombinedImageHash) -> ImagePair | None:
+    MATCH_THRESHOLD = 5
+    if img1.path != img2.path and abs(img1.hash - img2.hash) < MATCH_THRESHOLD:
+        return img1, img2
+    return None
+        
 
 class ANNFinder:
     pass
 
+type ImagePair = tuple[CombinedImageHash, CombinedImageHash]
+type ImageHashResult = tuple[CombinedImageHash, None] | tuple[None, str]
 
 class ImageHasher:
     size: int
@@ -93,11 +118,15 @@ class ImageHasher:
         self.logger = logger
         self.size = size
 
-    def create_hash_from_image(self, image_path: Path):
-        img = Image.open(image_path)
-        phash = self.global_phash(img)
-        crophash = self.crop_resistant_hash(img)
-        return CombinedImageHash(path=image_path, hash=phash, cropped_hash=crophash)
+    def create_hash_from_image(self, image_path: Path) -> ImageHashResult:
+        try:
+            img = Image.open(image_path)
+            phash = self.global_phash(img)
+            crophash = self.crop_resistant_hash(img)
+
+            return CombinedImageHash(path=image_path, hash=phash, cropped_hash=crophash), None
+        except (UnidentifiedImageError, OSError) as e:
+            return None, str(e)
 
     def alpharemover(self, image: Image.Image):
         if image.mode != 'RGBA':
